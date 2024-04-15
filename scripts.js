0ccecc34ade29cfe01bda225c58aadbc7c784557
Brittney Epperson <beppers87@hotmail.com>
2024-04-15 17:55:19 -0500

    Added read toggle and inverted styling. Fixed logic for readStatus when adding

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const haveRead = document.getElementById("haveRead");
+const haveNotRead = document.getElementById('haveNotRead');
 
 button.addEventListener("click", function() {
   addDialog.showModal();
@@ -15,7 +16,13 @@ button.addEventListener("click", function() {
 
 confirmBtn.addEventListener("click", function(event) {
   event.preventDefault();
-  addBooktoLibrary(title.value, author.value, pages.value, haveRead.value);
+  let readStatus;
+  if (haveRead.checked) {
+		readStatus = 'I have read';
+  } else {
+		readStatus = 'I have not read';
+  }
+  addBooktoLibrary(title.value, author.value, pages.value, readStatus);
   showBooks();
   addDialog.close();
 });
@@ -29,6 +36,10 @@ function Book(title, author, pages, haveRead) {
   this.haveRead = haveRead;
   this.info = function() {
     return `${title} by ${author}, ${pages} pages, ${haveRead}.`
+  };
+  this.toggleRead = function () {
+		this.haveRead =
+			this.haveRead === 'I have read' ? 'I have not read' : 'I have read';
   };
 }
 
@@ -61,17 +72,47 @@ cards.addEventListener("click", (event) => {
   }
 });
 
+function toggleReadStatus(event) {
+	const index = event.target.dataset.index;
+	myLibrary[index].toggleRead();
+	event.target.checked = myLibrary[index].haveRead === 'I have read';
+	showBooks();
+}
+
 function showBooks() {
   cards.innerHTML = "";
   for(let i = 0; i < myLibrary.length; i++) {
     const card = document.createElement("div");
     card.id = "card";
     card.textContent = myLibrary[i].info();
+    card.classList.toggle("unread", myLibrary[i].haveRead === "I have not read");
+
+    const readSlider = document.createElement("label");
+    readSlider.classList.add('read-slider', 'switch');
+
+    const readInput = document.createElement("input");
+    readInput.type = "checkbox";
+    readInput.checked = myLibrary[i].haveRead === "I have read";
+    readInput.dataset.index = i;
+    readInput.addEventListener("change", toggleReadStatus);
+    
+    const readSpan = document.createElement('span');
+  	readSpan.classList.add('slider', 'round');
+
+    readSlider.appendChild(readInput);
+    readSlider.appendChild(readSpan);
+
     const removeBtn = document.createElement("button");
-    removeBtn.textContent = "Remove";
-    removeBtn.classList.add("removeBtn");
+    removeBtn.textContent = "X";
+    removeBtn.classList.add("remove-btn");
     removeBtn.dataset.index = i;
+    removeBtn.addEventListener('click', () => {
+      myLibrary.splice(i, 1);
+      showBooks();
+    });
+
     card.appendChild(removeBtn);
+    card.appendChild(readSlider);
     cards.appendChild(card);
   }
 }
